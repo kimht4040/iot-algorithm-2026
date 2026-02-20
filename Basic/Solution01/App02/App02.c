57bb5e10574a7cb5d4fbeeeb2cb4b116dd7bdf80
김학태 <kimht4040@naver.com>
2026-02-20 13:16:59 +0900

    12일차 오전

--- Basic/Solution01/App02/App02.c
+++ Basic/Solution01/App02/App02.c
@@ -1,1 +1,51 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+
+int median(int a, int b, int c) {
+	
+	if (a >= b) {
+		if (b >= c) {
+			return b;
+
+		}
+		else if (a >= c) {
+			return c;
+		}
+		else
+			return a;
+	}
+	else {
+		if (a >= c) {
+			return a;
+
+		}
+		else if (b >= c) {
+			return c;
+		}
+		else
+			return b;
+	}
+
+
+	return 0;
+ }
+
+
+
+int main() {
+
+	
+	int a, b, c;
+
+	printf("세 정수 입력\n");
+	printf("a 값 > "); scanf("%d", &a);
+	printf("b 값 > "); scanf("%d", &b);
+	printf("c 값 > "); scanf("%d", &c);
+
+	printf("중앙 값은 %d\n ", median(a, b, c));
+
+
+	return 0;
+}
---
+++ Basic/algorithm01/App03/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#
+int main(void) {
+    printf("Hello11, World!\n");
+    return 0;
+}
